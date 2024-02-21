ff0366a4478479569602b84a64f04585c52f3cd1
Jack Hsu <jack.hsu@gmail.com>
2024-02-21 14:17:44 -0500

    chore(repo): enable e2e splitting for nx-dev-e2e

--- nx-dev/nx-dev-e2e/playwright.config.ts
+++ nx-dev/nx-dev-e2e/playwright.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
-import { nxE2EPreset } from '@nx/playwright/preset';
 
 // nx-ignore-next-line
-import { workspaceRoot } from '@nx/devkit';
-
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
-
-const preset = nxE2EPreset(__filename, { testDir: './src' });
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -18,10 +13,18 @@ const preset = nxE2EPreset(__filename, { testDir: './src' });
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  ...preset,
-  // CI default is 'dot', which doesn't show error output in CI.
-  // use list so errors are visible in CI logs
-  reporter: process.env.CI ? 'list' : preset.reporter,
+  testDir: './src',
+  outputDir: '../../dist/.playwright/nx-dev-e2e/test-output',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'list',
   // how long the entire suite can run, prevent CI from timing out
   globalTimeout: process.env.CI ? 1_800_000 : undefined,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -34,25 +37,14 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm exec nx run nx-dev:serve:production',
+    command: 'pnpm exec nx run nx-dev:next-start',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
-    cwd: workspaceRoot,
   },
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
     },
   ],
 });
